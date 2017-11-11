d2cd13414cf9c5932377d08c1b10ad378b90a786
ZouYouShun <itisalanlife@gmail.com>
2017-11-12 00:57:31 +0800

    add example for sortable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,37 @@ import { MatIconRegistry } from '@angular/material';
 })
 export class AppComponent implements OnInit {
 
+  avatars = [
+    {
+      name: '1',
+      image: 'http://semantic-ui.com/images/avatar2/large/matthew.png',
+      visible: false
+    },
+    {
+      name: '2',
+      image: 'http://semantic-ui.com/images/avatar/large/chris.jpg',
+      visible: false
+    },
+    {
+      name: '3',
+      image: 'http://semantic-ui.com/images/avatar/large/jenny.jpg',
+      visible: false
+    }, {
+      name: '4',
+      image: 'http://semantic-ui.com/images/avatar2/large/matthew.png',
+      visible: false
+    },
+    {
+      name: '5',
+      image: 'http://semantic-ui.com/images/avatar/large/chris.jpg',
+      visible: false
+    },
+    {
+      name: '6',
+      image: 'http://semantic-ui.com/images/avatar/large/jenny.jpg',
+      visible: false
+    },
+  ];
   ngOnInit(): void {
   }
 
